e62430dc7a6ea5d9b54ea61fa10c97f2b002e437
Aakanksha2812 <shindeaakanksha28@gmail.com>
2026-04-21 12:41:43 +0530

    DP Questions

--- src/DP/StringDP.java
+++ src/DP/StringDP.java
@@ -1,6 +1,5 @@
 package DP;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class StringDP {
@@ -19,6 +18,22 @@ public class StringDP {
         return dp[i][j];
     }
 
+    int largestCommonSubsequencesTab(String s1, String s2) {
+        int n=s1.length();
+        int m=s2.length();
+        int[][] dp=new int[n+1][m+1];
+        for (int i=n-1;i>=0;i--){
+            for (int j=m-1;j>=0;j--){
+                if (s1.charAt(i)==s2.charAt(j)){
+                    dp[i][j]=1+dp[i+1][j+1];
+                }else{
+                    dp[i][j]=Math.max(dp[i+1][j],dp[i][j+1]);
+                }
+            }
+        }
+        return dp[0][0];
+    }
+
     public static void main(String[] args) {
         StringDP d = new StringDP();
         String s1 = "abcde";
@@ -28,5 +43,7 @@ public class StringDP {
             Arrays.fill(arr, -1);
         }
         System.out.println("Longest Common Subsequence: " + d.largestCommonSubsequences(0, 0, s1, s2, dp));
+        System.out.println("Longest Common Subsequence: " + d.largestCommonSubsequencesTab( s1, s2));
+
     }
 }
